17c21a8a3b3353e1b1a66328981bcdc048efa916
darkkings19 <p.paredes06@ufromail.cl>
2022-03-23 09:36:40 -0300

    Initial commit

--- carrito.java
+++ carrito.java
@@ -1,5 +1,13 @@
+import java.util.Random;
+import  java.util.random.RandomGenerator;
 public class carrito {
     public static void main(String[] args) {
 
+        int[] carri = new int[args.length];
+        Random random = new Random();
+        for (int i = 0; i< carri.length ; i++) {
+            System.out.println(carri);
+
+        }
     }
 }
